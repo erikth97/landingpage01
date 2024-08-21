5692dba90cf901bfa9e0594f1cb8a9fc449d47ce
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-08-21 16:26:04 -0600

    Add: react scroll

---
+++ src/@types/react-scroll.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-scroll' {
+    export const Link: any;
+    export const Element: any;
+    export function animateScroll(options?: any): void;
+    export function scrollSpy(): void;
+    export function scroller(): void;
+  }
+  
